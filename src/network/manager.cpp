bc08fd8ef8989a96a97a86bd452588874be942f2
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2025-10-29 01:21:17 +0800

    Update manager.cpp

--- src/network/manager.cpp
+++ src/network/manager.cpp
@@ -1,7 +1,5 @@
 #include "manager.hpp"
-
 #include <utils.hpp>
-
 #include "packets/client.hpp"
 #include "packets/server.hpp"
 
@@ -9,10 +7,7 @@ time_t lastPinged = 0;
 
 NetworkManager::NetworkManager() {
     ix::initNetSystem();
-
-    // schedule pinging
     CCScheduler::get()->scheduleUpdateForTarget(this, -1, false);
-
     this->on<PongPacket>([this](PongPacket*) {
         responseTime = time(0) - lastPinged;
     });
@@ -20,33 +15,26 @@ NetworkManager::NetworkManager() {
 
 void NetworkManager::update(float dt) {
     if (!this->isConnected) return;
-
     if (time(0) - lastPinged < 10) return;
     lastPinged = time(0);
-
     this->send(PingPacket::create());
 }
 
 void NetworkManager::connect(bool shouldReconnect, std::function<void()> callback) {
     log::debug("connecting");
     if (this->isConnected && shouldReconnect) {
-        // disconnect then reconnect
         log::debug("already connected; disconnecting then reconnecting...");
         this->showDisconnectPopup = false;
         this->disconnect();
     }
 
-    socket.setUrl(
-        Mod::get()->getSettingValue<std::string>("server-url")
-    );
-
+    socket.setUrl(Mod::get()->getSettingValue<std::string>("server-url"));
     socket.disableAutomaticReconnection();
-
-    // socket.setPingInterval(15);
 
 #ifdef GEODE_IS_ANDROID
     ix::SocketTLSOptions tlsOptions;
     tlsOptions.caFile = (Mod::get()->getResourcesDir() / "cacert-2024-09-24.pem").string();
+    tlsOptions.disableCertVerification = true;
     socket.setTLSOptions(tlsOptions);
 #endif
 
@@ -55,69 +43,35 @@ void NetworkManager::connect(bool shouldReconnect, std::function<void()> callbac
             const auto errReason = msg->errorInfo.reason;
             log::error("ixwebsocket error: {}", errReason);
             Loader::get()->queueInMainThread([this, errReason]() {
-                FLAlertLayer::create(
-                    "CR Error",
-                    fmt::format("There was an error while connecting to the server: {}", errReason),
-                    "OK"
-                )->show();
+                FLAlertLayer::create("CR Error", fmt::format("There was an error while connecting to the server: {}", errReason), "OK")->show();
             });
             return;
         } else if (msg->type == ix::WebSocketMessageType::Open) {
             log::debug("connection success!");
-
-            // call middleware (this should run the provided cb)
             middleware([this, callback]() {
-                // register error packet
                 this->on<ErrorPacket>([](ErrorPacket* packet) {
-                    FLAlertLayer::create(
-                        "CR Error",
-                        fmt::format("The Creation Rotation server sent an error: <cy>{}</c>", packet->error).c_str(),
-                        "OK"
-                    )->show();
+                    FLAlertLayer::create("CR Error", fmt::format("The Creation Rotation server sent an error: <cy>{}</c>", packet->error).c_str(), "OK")->show();
                 });
-
-                // call the callback
                 callback();
-
-                // send all packets in queue
-                for (auto packetFn : packetQueue) {
-                    packetFn();
-                }
-
-                // clear the queue
+                for (auto packetFn : packetQueue) packetFn();
                 packetQueue.clear();
-
                 this->showDisconnectPopup = true;
                 Loader::get()->queueInMainThread([]() {
-                    Notification::create(
-                        "Connection sucessful!",
-                        NotificationIcon::Success,
-                        1.5f
-                    )->show();
+                    Notification::create("Connection sucessful!", NotificationIcon::Success, 1.5f)->show();
                 });
             });
-
             return;
         } else if (msg->type == ix::WebSocketMessageType::Close) {
             log::debug("connection closed");
             this->isConnected = false;
             const auto reason = msg->closeInfo.reason;
             Loader::get()->queueInMainThread([this, reason]() {
-                Notification::create(
-                    "Disconnected from Creation Rotation",
-                    NotificationIcon::Error,
-                    1.5f
-                )->show();
+                Notification::create("Disconnected from Creation Rotation", NotificationIcon::Error, 1.5f)->show();
                 if (disconnectEventCb) disconnectEventCb(reason);
                 if (this->showDisconnectPopup) {
-                    geode::createQuickPopup(
-                        "Disconnected",
-                        fmt::format("You have been disconnected from the Creation Rotation servers. Reason:\n\n{}", reason),
-                        "OK", "Close",
-                        [this, reason](auto, bool) {
-                            if (disconnectBtnCallback) disconnectBtnCallback(reason);
-                        }
-                    );
+                    geode::createQuickPopup("Disconnected", fmt::format("You have been disconnected from the Creation Rotation servers. Reason:\n\n{}", reason), "OK", "Close", [this, reason](auto, bool) {
+                        if (disconnectBtnCallback) disconnectBtnCallback(reason);
+                    });
                 }
             });
             return;
@@ -138,20 +92,16 @@ void NetworkManager::disconnect() {
 
 void NetworkManager::onMessage(const ix::WebSocketMessagePtr& msg) {
     auto strMsg = msg.get()->str;
-
     auto packetIdIdx = strMsg.find("|");
     if (packetIdIdx == std::string::npos) {
         log::error("invalid packet received");
         return;
     }
-
     auto packetStr = strMsg.substr(packetIdIdx + 1);
     auto packetId = geode::utils::numFromString<int>(strMsg.substr(0, packetIdIdx)).unwrapOr(0);
-
     if (!listeners.contains(packetId)) {
         log::error("unhandled packed ID {}", packetId);
         return;
     }
-
     listeners.at(packetId)(packetStr);
 }
